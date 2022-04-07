4eaf2ff3e5611d284aaa96ecfe009fda35626a86
mleich <mleich@athenian.org>
2022-04-07 12:30:11 -0700

    D per Day 1

--- src/print_inputD.py
+++ src/print_inputD.py
@@ -6,3 +6,22 @@
 #############################
 
 # Write code here:
+# print("Hello World!")
+# print("I am a stupid computer. I do whatever you tell me.")
+#
+# name = "Alexa"
+# print(f"Hello {name}!")
+# # print(name)
+#
+# name = input("Please enter your name: ")
+# print(f"Hello {name}!")
+#
+x = 8
+y = 7
+print(x * y)
+print(x ** y)
+
+num1 = float(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+num3 = int(input("Enter a 3rd number: "))
+print(num1 + num2 + num3)
